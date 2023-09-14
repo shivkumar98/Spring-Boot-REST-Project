b0a684c2e5a49a6f6401f24c23739f0fd081fba4
shiv.kumar <shiv_kumar.98@outlook.com>
2023-09-14 13:26:32 +0100

    Moved project into root

--- cruddemo_v2/src/main/java/com/luv2code/cruddemo/dao/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luv2code.cruddemo.dao;
-
-import com.luv2code.cruddemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
