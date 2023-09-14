41dfe2ac10f50ecc16dfc513e4da5455ea38aa07
shiv.kumar <shiv_kumar.98@outlook.com>
2023-09-14 13:31:30 +0100

    Created unit test

--- src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/cruddemo/CruddemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.entity.Employee;
+import com.luv2code.cruddemo.service.EmployeeServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CruddemoApplicationTests {
 
+	@Mock
+	private EmployeeServiceImpl employeeService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldCreateEmployee() {
+		Employee employee = new Employee();
+		employeeService.save(employee);
+		employeeService.deleteById(employee.getId());
 	}
 
 }
